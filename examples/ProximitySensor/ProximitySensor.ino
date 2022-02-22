762cef5579e18a4eac9d740f0e7ab298be4669dc
Goran Jurić <74900926+gogi640@users.noreply.github.com>
2022-02-22 14:14:26 +0100

    Update ProximitySensor.ino

--- examples/ProximitySensor/ProximitySensor.ino
+++ examples/ProximitySensor/ProximitySensor.ino
@@ -1,14 +1,17 @@
-/*
-  APDS-9960 - Proximity Sensor
-
-  This example reads proximity data from the on-board APDS-9960 sensor of the
-  Nano 33 BLE Sense and prints the proximity value to the Serial Monitor
-  every 100 ms.
-
-  This example code is in the public domain.
-
-  Modified by soldered.com for https://solde.red/333047
-*/
+/**
+ **************************************************
+ *
+ * @file        ProximitySensor.ino
+ * @brief       This example reads proximity data from the on-board APDS-9960 sensor of the
+ *              Nano 33 BLE Sense and prints the proximity value to the Serial Monitor
+ *              every 100 ms.
+ *
+ *              This example code is in the public domain.
+ *    
+ *              Product: https://solde.red/333047
+ *
+ *              Modified by soldered.com
+ ***************************************************/
 
 #include "APDS9960-SOLDERED.h"
 
@@ -16,13 +19,14 @@ APDS_9960 APDS;
 
 void setup()
 {
-    Serial.begin(115200);
-    while (!Serial)
+    Serial.begin(115200); //Begin serial communication with PC
+    while (!Serial) //Wait for serial to become active
         ;
 
-    if (!APDS.begin())
+    if (!APDS.begin())  //Begin communication with sensor
     {
-        Serial.println("Error initializing APDS-9960 sensor!");
+      Serial.println("Error initializing APDS-9960 sensor!");
+      while(1);
     }
 }
 
